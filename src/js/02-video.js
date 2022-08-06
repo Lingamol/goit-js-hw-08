191ff58acec0b449d637c18673cc5103d8cbdfbf
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-08-06 23:56:10 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  save('videoplayer-current-time', data);
+}
+
+player
+  .setCurrentTime(
+    load('videoplayer-current-time')
+      ? load('videoplayer-current-time').seconds
+      : 0
+  )
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  formText: document.querySelector('textarea[name="message"]'),
+  inputEmail: document.querySelector('input[type="email"]'),
+};
+
+refs.form.addEventListener('input', throttle(onInputForm, 300));
+
+refs.form.addEventListener('submit', onSubmitForm);
+
+loadFormData();
+
+function onInputForm(event) {
+  formData[event.target.name] = event.target.value;
+  save(LOCALSTORAGE_KEY, formData);
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log('reset form');
+}
+function loadFormData() {
+  const localFormData = load(LOCALSTORAGE_KEY);
+  if (localFormData) {
+    refs.formText.textContent = localFormData.message || '';
+    refs.inputEmail.value = localFormData.email || '';
+  }
+}
