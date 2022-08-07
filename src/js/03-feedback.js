bb60b129c7cb4f5bc707349ef58e4a6325267867
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-08-07 22:09:27 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,29 +8,35 @@ const refs = {
   formText: document.querySelector('textarea[name="message"]'),
   inputEmail: document.querySelector('input[type="email"]'),
 };
+refs.form.addEventListener('submit', onSubmitForm);
 
 refs.form.addEventListener('input', throttle(onInputForm, 500));
-
-refs.form.addEventListener('submit', onSubmitForm);
 
 loadFormData();
 
 function onInputForm(event) {
   formData[event.target.name] = event.target.value;
   save(LOCALSTORAGE_KEY, formData);
+  console.log('input', formData);
 }
 
 function onSubmitForm(event) {
   event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return console.log('Please fill in all the fields!');
+  }
+  console.log({ email: email.value, message: message.value });
   event.currentTarget.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log(formData);
 }
 function loadFormData() {
   const localFormData = load(LOCALSTORAGE_KEY);
   if (localFormData) {
-    refs.formText.textContent = localFormData.message || '';
+    refs.formText.value = localFormData.message || '';
     refs.inputEmail.value = localFormData.email || '';
-    // console.log('load', localFormData);
   }
 }
